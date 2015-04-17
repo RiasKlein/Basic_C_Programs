3915ecb0e9fc17067fe5036ef4ee19e02617cfe3
Shun Tse <shunmantse@gmail.com>
2015-04-17 02:34:01 -0400

    Hello World
    a hello world first program has been added

---
+++ hello/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>	// printf
+#include <stdint.h>	// uint64
+#include <stdlib.h> // exit()
+#include <math.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hello World!\n");
+
+	getchar();
+	return 0;
+}
