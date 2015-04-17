110ba6b1668ef5e40f597eaa33c2427d44eed3c3
Shun Tse <shunmantse@gmail.com>
2015-04-17 11:52:36 -0400

    completed queue
    - interactive queue with basic functions complete
    - enqueue / dequeue / printing / size stats
    - no further features are planned for this interactive queue

--- queue/queue.c
+++ queue/queue.c
@@ -23,21 +23,25 @@ typedef struct {
 } Queue;
 
 /**************************************/
-/* Commands */
+/* Command Flags */
 int exit_flag = 0;
 int help_flag = 0;
 int enqueue_flag = 0;
 int print_flag = 0;
+int dequeue_flag = 0;
+int flush_flag = 0;
+int size_flag = 0;
 
 /**************************************/
 /* Global Variables */
-Queue queue;
-int char_count;
-int queueSize;
+Queue queue;				// queue 
+int queueSize;				// max capacity of the queue
+int char_count;				// 
 char buffer[MAXBUFFERSIZE];	// buffer to hold user input
 char item[MAXBUFFERSIZE];	// buffer to hold enqueue items
 
 /**************************************/
+/* Function to confirm command line input */
 int checkCommand(char* command_buffer, int buffer_start, char* command, int command_size) {
 	int i;
 
@@ -52,51 +56,100 @@ int checkCommand(char* command_buffer, int buffer_start, char* command, int comm
 /**************************************/
 /* Help */
 void printHelp() {
-	printf("\n\t\tHelp\n\n");
-	printf("dequeue\t\tremoves element from queue and prints it to screen\n");
-	printf("enqueue (input)\tinserts element into queue\n");
+	printf("\n\tHelp\n");
+	printf("/dequeue\tremoves element from queue\n");
+	printf("/enqueue (item)\tinserts element into queue\n");
 	printf("help\t\tprovides help info\n");
-	printf("print\t\tprints out queue\n");
-	printf("quit\t\texits program\n");
-	printf("reset\t\tzeroes the queue\n");
-	printf("size\t\tprints out size of queue\n");
+	printf("/print\t\tprints out queue\n");
+	printf("/quit\t\texits program\n");
+	printf("/flush\t\tempties the queue\n");
+	printf("/size\t\tprints out size of queue\n");
+
+	printf("\n\tShortcuts\n");
+	printf("/d\t\tdequeue\n");
+	printf("/e (item)\tenqueue\n");
+	printf("/p\t\tprint\n");
+	printf("/q\t\tquit\n");
+	printf("/f\t\tflush\n");
+	printf("/s\t\tsize\n");
 	printf("\n");
 }
 
 /**************************************/
+/* Function to enqueue into queue */
 void enqueue() {
-	int i, j, k;
+	int i, j, k, c;
 	char ch;
 
-	// Getting the user's item
-	i = 8;
-	j = 0;
+	if (queue.num < queueSize) {
 
-	ch = buffer[i];
-	while ((ch != ' ') && (i < char_count)) {
-		item[j] = ch;
-		i++;
+		// Check how the user enqueued (short cut or not)
+		c = checkCommand(buffer, 0, "/enqueue ", 9);
+		if (c == 1) {
+			// The user did not use the shortcut
+			i = 9;
+			j = 0;
+		}
+		else {
+			// The user used the shortcut e.g. e item
+			i = 3;
+			j = 0;
+		}
+
 		ch = buffer[i];
-		j++;
+		while ((ch != ' ') && (i < char_count)) {
+			item[j] = ch;
+			i++;
+			ch = buffer[i];
+			j++;
+		}
+		item[j] = 0x00;
+
+		sscanf(item, "%d", &k);
+
+		printf("Enqueuing Item: %d\n", k);
+
+		queue.num += 1;
+		queue.queue[queue.back] = k;
+		queue.back = (queue.back + 1) % queueSize;
 	}
-	item[j] = 0x00;
-
-	sscanf(item, "%d", &k);
-
-	queue.num += 1;
-	queue.queue[queue.back] = k;
-	queue.back += 1;
+	else {
+		printf("The queue is full. Cannot enqueue.\n");
+	}
 }
 
 /**************************************/
+/* Function to dequeue from queue */
+void dequeue() {
+	int i;
+
+	if (queue.num > 0) {
+		printf("Deqeued Item: %d\n", queue.queue[queue.front]);
+		queue.front = (queue.front + 1) % queueSize;
+		queue.num -= 1;
+	}
+	else {
+		printf("The queue is empty. Cannot dequeue.\n");
+	}
+
+}
+
+/**************************************/
+/* Function to print out queue contents */
 void printQueue(){
 	int i;
 
 	printf("Printing Queue:\n");
-	for (i = 0; i < queue.num; i++) {
-		printf("%d ", queue.queue[(queue.front + i) % queueSize]);
+	if (queue.num == 0) {
+		printf("The queue is empty right now.\n");
 	}
-	printf("\n\n");
+	else {
+		for (i = 0; i < queue.num; i++) {
+			printf("%d ", queue.queue[(queue.front + i) % queueSize]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
 
 /**************************************/
@@ -105,13 +158,13 @@ int main(int argc, char* argv[]) {
 	char ch;
 
 	//===== SETTING UP OUR QUEUE
-	// See if a size was included via command line for the queue otherwise, it's the size of MAXBUFFERSIZE
+	//=== See if a size was included via command line for the queue otherwise, it's the size of MAXBUFFERSIZE
 	queueSize = MAXBUFFERSIZE;
 	if (argc > 1) {
 		sscanf(argv[1], "%d", &queueSize);
 	}
 
-	// Initializing values
+	//=== Initializing values of queue
 	queue.front = 0;
 	queue.back = 0;
 	queue.num = 0;
@@ -142,19 +195,49 @@ int main(int argc, char* argv[]) {
 		//=== Check what the command is and how to handle it
 		int c;
 		
+		c = checkCommand(buffer, 0, "/quit", 5);
+		if (c == 1) { exit_flag = 1; }
 		c = checkCommand(buffer, 0, "quit", 4);
 		if (c == 1) { exit_flag = 1; }
+		c = checkCommand(buffer, 0, "/q\0", 3);
+		if (c == 1) { exit_flag = 1; }
 
+		c = checkCommand(buffer, 0, "/help\0", 6);
+		if (c == 1) { help_flag = 1; }
 		c = checkCommand(buffer, 0, "help", 4);
 		if (c == 1) { help_flag = 1; }
 
-		c = checkCommand(buffer, 0, "enqueue", 7);
+		c = checkCommand(buffer, 0, "/enqueue ", 9);
+		if (c == 1) { enqueue_flag = 1; }
+		c = checkCommand(buffer, 0, "/e ", 3);
 		if (c == 1) { enqueue_flag = 1; }
 
-		c = checkCommand(buffer, 0, "print", 5);
+		c = checkCommand(buffer, 0, "/dequeue\0", 9);
+		if (c == 1) { dequeue_flag = 1; }
+		c = checkCommand(buffer, 0, "/d\0", 3);
+		if (c == 1) { dequeue_flag = 1; }
+
+		c = checkCommand(buffer, 0, "/print\0", 7);
 		if (c == 1) { print_flag = 1; }
+		c = checkCommand(buffer, 0, "/p\0", 3);
+		if (c == 1) { print_flag = 1; }
+
+		c = checkCommand(buffer, 0, "/flush\0", 7);
+		if (c == 1) { flush_flag = 1; }
+		c = checkCommand(buffer, 0, "/f\0", 3);
+		if (c == 1) { flush_flag = 1; }
 	
+		c = checkCommand(buffer, 0, "/size\0", 6);
+		if (c == 1) { size_flag = 1; }
+		c = checkCommand(buffer, 0, "/s\0", 3);
+		if (c == 1) { size_flag = 1; }
+
 		//=====	HANDLING COMMAND
+
+		//=== Handling invalid input
+		if (!help_flag && !enqueue_flag && !dequeue_flag && !print_flag && !flush_flag && !size_flag && !exit_flag) {
+			printf("Invalid Input. Use \"help\" to see available commands.\n");
+		}
 
 		//=== Handling the help flag
 		if (help_flag) {
@@ -170,6 +253,13 @@ int main(int argc, char* argv[]) {
 			enqueue_flag = 0; // Reset enqueue flag since we handled it
 		}
 
+		//=== Handling the dequeue flag
+		if (dequeue_flag) {
+			dequeue();
+
+			dequeue_flag = 0; // Reset the dequeue flag
+		}
+
 		//=== Handling the print flag
 		if (print_flag) {
 			printQueue();
@@ -177,10 +267,30 @@ int main(int argc, char* argv[]) {
 			print_flag = 0; // Reset the print flag
 		}
 
+		//=== Handling the flush flag
+		if (flush_flag) {
+			queue.front = 0;
+			queue.back = 0;
+			queue.num = 0;
+
+			printf("The queue has been emptied.\n");
+
+			flush_flag = 0;
+		}
+
+		//=== Handling the size flag
+		if (size_flag) {
+			printf("Queue Max Size: %d\n", queueSize);
+			printf("Number of items: %d\n", queue.num);
+
+			size_flag = 0;
+		}
+
 		//===== GET READY TO ACCEPT NEXT COMMAND
-		//printf("char_count: %d buffer: %s\n", char_count, buffer);
 		char_count = 0;
 	}
+
+	printf("\nExiting interactive queue program.\n");
 
 	exit(0);
 	return 0;
